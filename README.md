61d9ce4f888a87626e4961917a959c5a1a136d31
orgest-pulaj <63362291+orgest-pulaj@users.noreply.github.com>
2021-01-22 16:29:23 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-hhh
+
+This repo holds the code for the free public Silex instance hosted by Silex Labs foundation
+
+[Please use the main Silex repository](https://github.com/silexlabs/Silex/) for docs and issues
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const { SilexServer, Config } = require('silex-website-builder')
+const serveStatic = require('serve-static')
+const path = require('path')
+const got = require('got')
+
+const config = new Config()
+const silex = new SilexServer(config)
+
+// logs for debugging prod version
+silex.app.use('/static', (req, res, next) => {
+  const splitted = req.url.split('/')
+  const version = splitted[1]
+  const filename = splitted.pop()
+  const referer = req.header('Referer')
+  if(filename === 'jquery.js') {
+    trackEvent('static-file-request', version, referer, 0)
+    .then(response => {
+      console.log('logged in alanytics SUCCESS:', version, filename, referer)
+    })
+    .catch(err => {
+      console.error('logged in alanytics ERROR:', version, filename, referer, err)
+    })
+  }
+  next()
+})
+
+// serve modified html
+silex.app.use('/', serveStatic(path.resolve('./dist/')))
+
+silex.start(function() {
+  console.log('server started')
+})
+
+const { GA_TRACKING_ID } = require('./env.js')
+function trackEvent (category, action, label, value) {
+  console.log('logging in alanytics:', {GA_TRACKING_ID, category, action, label, value})
+  const data = {
+    // API Version.
+    v: '1',
+    // Tracking ID / Property ID.
+    tid: GA_TRACKING_ID,
+    // Anonymous Client Identifier. Ideally, this should be a UUID that
+    // is associated with particular user, device, or browser instance.
+    cid: '555',
+    // Event hit type.
+    t: 'event',
+    // Event category.
+    ec: category,
+    // Event action.
+    ea: action,
+    // Event label.
+    el: label,
+    // Event value.
+    ev: value,
+    z: Math.round(Math.random() * 9999),
+  }
+
+  return got.post('http://www.google-analytics.com/collect', {
+    form: data
+  })
+}
